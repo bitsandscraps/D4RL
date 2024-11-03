5f494588327ee4e97d98c271ea0800545c797f10
bitsandscraps <17763502+bitsandscraps@users.noreply.github.com>
2024-11-04 00:35:16 +0900

    change bullet to extra

--- setup.py
+++ setup.py
@@ -62,11 +62,11 @@ setup(
         "gym<0.24.0",
         "numpy",
         "mujoco_py",
-        "pybullet",
         "h5py",
         "termcolor",  # adept_envs dependency
         "click",  # adept_envs dependency
         "dm_control>=1.0.3",
         "mjrl @ git+https://github.com/aravindr93/mjrl@master#egg=mjrl",
     ],
+    extras_require={"pybullet": ["pybullet"]},
 )
